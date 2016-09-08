17aac45f48b501d694fa5c572b9ae84f2904dcf2
michael <micsol95@gmail.com>
2016-09-09 00:40:52 +0300

    finished

---
+++ module1-solution/js/app.js
@@ -0,0 +1,36 @@
+(function () {
+  "use strict";
+
+  var app = angular.module('LunchCheck', []);
+
+  app.controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.check = function () {
+      var count = countItems($scope.lunch);
+
+      if (!count) {
+        $scope.message = "Please enter data first";
+      } else {
+        if (count <= 3) {
+          $scope.message = "Enjoy!";
+        }
+        else {
+          $scope.message = "Too much!";
+        }
+      }
+    };
+  }
+
+  function countItems(lunch) {
+    if (lunch) {
+      lunch = lunch.split(',').map(function (str) {
+        return str.trim();
+      });
+
+      return lunch.length;
+    }
+  }
+})();
